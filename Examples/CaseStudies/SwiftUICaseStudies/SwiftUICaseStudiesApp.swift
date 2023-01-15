bcbba68adcc39325a50c3c1b448ce2cfd4920929
harry <hkjlee109@gmail.com>
2023-01-15 17:18:33 +0100

    Add demo projects

---
+++ Examples/CaseStudies/SwiftUICaseStudies/SwiftUICaseStudiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICaseStudiesApp.swift
+//  SwiftUICaseStudies
+//
+//  Created by Harry Lee on 13.01.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICaseStudiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
