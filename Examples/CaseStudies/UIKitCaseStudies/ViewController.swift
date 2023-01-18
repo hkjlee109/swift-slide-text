a5484a2354d8dca7de0bfdf8159223c491977d68
harry <hkjlee109@gmail.com>
2023-01-18 19:46:34 +0100

    Setup UIKit demo

--- Examples/CaseStudies/UIKitCaseStudies/ViewController.swift
+++ Examples/CaseStudies/UIKitCaseStudies/ViewController.swift
@@ -1,12 +1,49 @@
 import UIKit
+import UISlideText
+
+
+//import SlideText
+//
+//class ViewController: UIViewController {
+//
+//    private let slideText = UISlideTextController("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla diam eros, mollis nec faucibus ac, fermentum eu augue.")
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        addChild(slideText)
+//        view.addSubview(slideText.view)
+//
+//        slideText.view.translatesAutoresizingMaskIntoConstraints = false
+//
+//        NSLayoutConstraint.activate([
+//            slideText.view.topAnchor.constraint(equalTo: view.topAnchor),
+//            slideText.view.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+//            slideText.view.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+//            slideText.view.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+//        ])
+//    }
+//
+//}
+
 
 class ViewController: UIViewController {
 
+    private let slideText = UISlideText()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        view.addSubview(slideText)
+
+        slideText.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([
+            slideText.topAnchor.constraint(equalTo: view.topAnchor),
+            slideText.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+            slideText.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            slideText.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+        ])
     }
 
-
 }
-
--- Sources/UISlideText/UISlideText.swift
+++ Sources/UISlideText/UISlideText.swift
@@ -1,8 +1,15 @@
-//
-//  File.swift
-//  
-//
-//  Created by Harry Lee on 17.01.23.
-//
+import UIKit
 
-import Foundation
+public class UISlideText: UIView {
+
+    public init() {
+        super.init(frame: .zero)
+
+        backgroundColor = .cyan
+    }
+
+    @available(*, unavailable)
+    public required init?(coder _: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
